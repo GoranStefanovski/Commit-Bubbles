055cb88b6c5934dc7b3843ae0c78fb353357ce44
Goran Stefanovski <tevidma@gmail.com>
2023-02-25 14:36:19 +0100

    add modal to seperate component

---
+++ comps/SVG/modal.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'modal',
+  props: {
+    pickedAuthorObject: Object
+  },
+  methods: {
+    setAuthorZero() {
+        this.$emit('setAuthor')
+    },
+    commitDate(ts) {
+      return moment(new Date(ts)).format('MM/DD/yy HH:mm:ss')
+    }
+  }
+}
+</script>
+
+<template>
+    <g font-family="Verdana" font-size="16" @click="setAuthorZero()">
+      <rect style="stroke:rgb(0,0,0);stroke-width:2" x="300" y="300" height="300" width="400" fill="white"
+            opacity="0.7"/>
+      <text class="close-btn" @click="setAuthorZero()" x="55%" y="330">X</text>
+      <text x="320" y="330">Selected Commit</text>
+      <text x="320" y="380">Commit:</text>
+      <foreignObject x="395" y="363" width="900" height="100">
+        <a _target="_blank" v-bind:href="pickedAuthorObject.commitUrl">
+          {{ pickedAuthorObject.message }}
+        </a>
+      </foreignObject>
+      <text x="320" y="430">Author: {{ pickedAuthorObject.author }}</text>
+      <text x="320" y="480">Date/Time: {{ commitDate(pickedAuthorObject.ts) }}</text>
+      <text x="320" y="530">Size: {{ pickedAuthorObject.size }}</text>
+      <text x="320" y="580">Test Percentage: {{ pickedAuthorObject.testPercentage }}</text> 
+    </g>
+</template>
+
+<style scoped>
+.close-btn:hover {
+  cursor: pointer;
+}
+</style>
--- comps/SVG/svg-comp.vue
+++ comps/SVG/svg-comp.vue
@@ -1,7 +1,11 @@
 <script>
+import SkModal from './modal.vue'
 export default {
   name: 'svg-comp',
   props: {
+  },
+  components: {
+    SkModal,
   },
   directives: {},
   data() {
@@ -50,7 +54,6 @@ export default {
       .then(response => response.json())
       .then(data => {
         this.commits = data;
-        console.log(this.commits)
         this.selectedAuthors = this.authors;
       });
   },
@@ -65,9 +68,6 @@ export default {
     cleanAllAuthors() {
       this.selectedAuthors = [];
     },
-    commitDate(ts) {
-      return moment(new Date(ts)).format('MM/DD/yy HH:mm:ss')
-    }
   }
 }
 </script>
@@ -119,22 +119,8 @@ export default {
       </div>
       </body>
     </foreignObject>
-    <g font-family="Verdana" font-size="16" v-show="clickedAuthor" @click="clickedAuthor = 0">
-      <rect style="stroke:rgb(0,0,0);stroke-width:2" x="300" y="300" height="300" width="400" fill="white"
-            opacity="0.7"/>
-      <text class="close-btn" @click="clickedAuthor = 0" x="55%" y="330">X</text>
-      <text x="320" y="330">Selected Commit</text>
-      <text x="320" y="380">Commit:</text>
-      <foreignObject x="395" y="363" width="900" height="100">
-        <a _target="_blank" v-bind:href="pickedAuthorObject.commitUrl">
-          {{ pickedAuthorObject.message }}
-        </a>
-      </foreignObject>
-      <text x="320" y="430">Author: {{ pickedAuthorObject.author }}</text>
-      <text x="320" y="480">Date/Time: {{ commitDate(pickedAuthorObject.ts) }}</text>
-      <text x="320" y="530">Size: {{ pickedAuthorObject.size }}</text>
-      <text x="320" y="580">Test Percentage: {{ pickedAuthorObject.testPercentage }}</text>
-    </g>
+    
+    <sk-modal v-show="clickedAuthor" :pickedAuthorObject="pickedAuthorObject" @setAuthor="clickedAuthor = 0"></sk-modal>
   </svg>
 </template>
 
